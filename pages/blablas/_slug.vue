de33fb8532700694b640b777f93e394c0e701d2f
Inani El Houssain <inanielhoussain@gmail.com>
2019-05-05 21:34:35 +0000

    fb video player

---
+++ pages/blablas/_slug.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="fb-video" data-href="https://www.facebook.com/facebook/videos/10153231379946729/" data-width="500" data-show-text="false">
+      <div class="fb-xfbml-parse-ignore">
+        <blockquote cite="https://www.facebook.com/facebook/videos/10153231379946729/">
+          <a href="https://www.facebook.com/facebook/videos/10153231379946729/">How to Share With Just Friends</a>
+          <p>How to share with just friends.</p>
+          Posted by <a href="https://www.facebook.com/facebook/">Facebook</a> on Friday, December 5, 2014
+        </blockquote>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  head () {
+    return {
+      script: [
+        { src: 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2' }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
