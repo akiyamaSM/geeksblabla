1df9e9fdc50f784894441a10df1531f259efd023
Inani El Houssain <inanielhoussain@gmail.com>
2019-05-09 22:21:34 +0000

    About responsive

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="container mx-auto flex items-center font-sans flex-1">
-    <img class="mr-12" src="https://geeksblabla.com/static/8af9cd56d5bac8799dbbce76f196d132/3fa08/about.png" alt="Community">
+  <div class="container mx-auto flex flex-col md:flex-row justify-center items-center font-sans md:flex-1">
+    <img class="md:mr-12" src="https://geeksblabla.com/static/8af9cd56d5bac8799dbbce76f196d132/3fa08/about.png" alt="Community">
     <div class="flex flex-col">
       <h1 class="my-12 text-white font-bold text-4xl">We Believe Community Matters</h1>
       <div class="paragraphs text-grey text-xs leading-loose">
@@ -8,7 +8,7 @@
         <p>We Invite knowledgable and interesting people who are not always known to the public so that they share with us their experience.</p>
         <p>We talk about the hottest new topics and explain it to the community in a way that is simple and approachable.</p>
       </div>
-      <a class="geek-button self-start" href="/blablas">See All Blablas</a>
+      <a class="geek-button block md:self-start" href="/blablas">See All Blablas</a>
     </div>
   </div>
 </template>
